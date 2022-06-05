392a07f0d4f82796fe78aca6a63749d0e9a1c432
rishabgaddi <rishab03@live.com>
2022-06-05 16:59:22 +0200

    Added the feature to connect to a metamask account.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import Body from "./components/Body";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Body />
     </div>
   );
 }
