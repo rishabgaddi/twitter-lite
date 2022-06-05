d08d6cff2c9a0c772d00245978effa754b09e33e
rishabgaddi <rishab03@live.com>
2022-06-05 22:50:18 +0200

    Changes/Fixes in .env and .prettierrc

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,11 +1,10 @@
 module.exports = {
   printWidth: 80,
   tabWidth: 2,
-  singleQuote: true,
-  jsxSingleQuote: false,
+  semi: true,
+  jsxBracketSameLine: false,
   trailingComma: "all", // https://prettier.io/docs/en/options.html#trailing-commas
   bracketSpacing: true, // https://prettier.io/docs/en/options.html#bracket-spacing
-  bracketSameLine: false, // https://prettier.io/docs/en/options.html#bracket-line
   arrowParens: "always", // https://prettier.io/docs/en/options.html#arrow-function-parentheses
   proseWrap: "never", // https://prettier.io/docs/en/options.html#prose-wrap
   htmlWhitespaceSensitivity: "strict", // https://prettier.io/docs/en/options.html#html-whitespace-sensitivity
