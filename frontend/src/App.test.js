9fe41f21b5e4fb0baf546159b737c8f5d63714a5
rishabgaddi <rishab03@live.com>
2022-06-06 11:25:06 +0200

    Removed the frontend from here and added it to the twitter-lite-frontend repository.

--- frontend/src/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("Renders Header component", () => {
-  render(<App />);
-  expect(screen.getByText("Welcome to Twitter Lite")).toBeInTheDocument();
-});
--- frontend/src/components/Header.js
+++
@@ -1,4 +0,0 @@
-function Header() {
-  return <h1>Welcome to Twitter Lite</h1>;
-}
-export default Header;
